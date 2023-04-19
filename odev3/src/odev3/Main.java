7267ff6185d5cc71482d4c8262a572b9e65635cd
Yiğit Coşkun <70442363+yigiitcoskun@users.noreply.github.com>
2023-04-19 13:19:54 +0300

    Update Main.java

--- odev3/src/odev3/Main.java
+++ odev3/src/odev3/Main.java
@@ -13,7 +13,7 @@ public class Main {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
-		customerManager.save(new Customer(1,"Mustafa Yiğit","Coşkun", new Date(2003,9,11), "14435414950"));
+		customerManager.save(new Customer(1,"Mustafa Yiğit","Coşkun", new Date(2003,9,11), "15350403285"));
 	
 		BaseCustomerManager customerManager2 = new NeroCustomerManager(new MernisServiceAdapter());
 		customerManager2.save(new Customer(2,"Engin", "Demiroğ", new Date(1985,1,6), "2113323232"));
